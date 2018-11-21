b112debf45fbc2d2224d1f4ec3adaf8acf0c3013
unknown <Administrator@PC-APP-YANGRX.hzecrf.com>
2018-11-21 10:05:04 +0800

    添加项目文件。

---
+++ MyCoreTest/Controllers/ApiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyCoreTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult Get()
+        {
+            var result = Add(2,3);
+            return new JsonResult("OK");
+        }
+
+        public int Add(int a, int b) => a + b;
+
+    }
+}
