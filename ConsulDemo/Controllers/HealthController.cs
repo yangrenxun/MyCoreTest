9076bbc26c85a4acf4dfa151582ecc76be979e6d
Renxun Yang <yangrenxun@outlook.com>
2018-12-28 15:33:47 +0800

    consul demo

---
+++ ConsulDemo/Controllers/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsulDemo.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("ok");
+    }
+}
